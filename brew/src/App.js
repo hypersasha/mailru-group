560436d88dd8821ce4bc842548cbfddcb0521e2c
Alexander Belov <callmistercat@gmail.com>
2019-05-11 17:52:37 +0300

    New main styles and css custom props.

--- brew/src/App.js
+++ brew/src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {platform, IOS} from './lib/platform';
 const osname = platform();
 
-import './test.less';
+import './App.css'; // This is IMPORTANT import.
+import './test.less' // Delete this on production.
 
 class App extends Component {
 
@@ -14,6 +15,8 @@ class App extends Component {
             <div className="App">
                 <div className="logo">
                     <img src="https://cdn.dribbble.com/users/85713/screenshots/3775213/herbal_dribble.gif" width={200} alt="Green Tea"/>
+                    <h1>Заварилось!</h1>
+                    <p>Если Вы видите этот экран, значит все <b>заварилось</b> успешно!</p>
                 </div>
             </div>
         );
--- brew/webpack.config.js
+++ brew/webpack.config.js
@@ -25,8 +25,20 @@ module.exports = {
                         }
                     }
                 ]
-            }
+            },
             // TODO: make rules for fonts.
+            {
+                test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/fonts/'
+                        }
+                    }
+                ]
+            }
         ]
     }
 };
